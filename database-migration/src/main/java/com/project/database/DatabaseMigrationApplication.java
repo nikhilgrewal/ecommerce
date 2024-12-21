ae8966507a54ab120f2f8a0d085d5a1c065b1508
nikhilgrewal <nikhilgrewal000@gmail.com>
2024-12-21 14:51:29 +0530

    Added some dependencies

--- database-migration/src/main/java/com/project/database/DatabaseMigrationApplication.java
+++ database-migration/src/main/java/com/project/database/DatabaseMigrationApplication.java
@@ -1,4 +1,4 @@
-package com.project;
+package com.project.database;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
--- manager/src/main/java/com/project/EcommerceApplication.java
+++ manager/src/main/java/com/project/EcommerceApplication.java
@@ -3,7 +3,7 @@ package com.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.project")
 public class EcommerceApplication {
 
     public static void main(String[] args) {
